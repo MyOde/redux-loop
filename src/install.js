928f0ff4dd26332b09e4fa6600be56efa028122b
Toms Ozols <t.ozols@jobtender24.com>
2019-04-30 13:23:57 +0200

    I don't know

--- src/install.js
+++ src/install.js
@@ -55,7 +55,7 @@ export function install(config = {}) {
 
     function dispatch(action) {
       if (action === 'function') {
-        return action(dispatch, store.getState);
+        return action(store.dispatch, store.getState);
       }
 
       const result = store.dispatch(action);
